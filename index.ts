e62770eaf36560cd2ba6a8fd57ce4aaeb7c6289a
Ahmed Fasih <wuzzyview@gmail.com>
2020-07-19 20:44:38 -0400

    API tokens via bearer authentication

--- index.ts
+++ index.ts
@@ -3,9 +3,19 @@ import {isRight} from 'fp-ts/lib/Either';
 import * as t from 'io-ts';
 import passport from 'passport';
 import GitHubStrategy from 'passport-github';
+import {Strategy as BearerStrategy} from 'passport-http-bearer';
 
 import {setup, sync} from './sync';
-import {findOrCreateGithub, getUser, IUser} from './users';
+import {
+  createApiToken,
+  deleteAllApiTokens,
+  deleteApiToken,
+  findApiToken,
+  findOrCreateGithub,
+  getAllApiTokenNames,
+  getUser,
+  IUser
+} from './users';
 
 const app = express();
 const port = 3000;
@@ -24,9 +34,11 @@ passport.use(new GitHubStrategy(
       clientSecret: env.GITHUB_CLIENT_SECRET,
       callbackURL: `http://127.0.0.1:${port}/auth/github/callback`,
     },
-    // This verify function converts the GitHub profile into our app's object representing the user (IUser)
-    function verify(accessToken, refreshToken, profile,
-                    cb) { findOrCreateGithub(profile).then(ret => cb(null, ret)); }));
+    // This function converts the GitHub profile into our app's object representing the user (IUser)
+    (accessToken, refreshToken, profile, cb) => findOrCreateGithub(profile).then(ret => cb(null, ret))));
+// Tell Passport we want to use Bearer (API token) auth, and *name* this strategy: we'll use this name below
+passport.use('bearer', new BearerStrategy((token, cb) => findApiToken(token).then(ret => cb(null, ret ? ret : false))));
+
 // Serialize an IUser into something we'll store in the user's session (very tiny)
 passport.serializeUser(function(user: IUser, cb) { cb(null, user.gotandaId); });
 // Take the data we stored in the session (`gotandaId`) and resurrect the full IUser object
@@ -58,6 +70,26 @@ const ensureUnauthenticated: RequestHandler = (req, res, next) => {
   next();
 };
 
+// The name "bearer" here matches the name we gave the strategy above. See
+// https://dsackerman.com/passportjs-using-multiple-strategies-on-the-same-endpoint/
+const bearerAuthentication = passport.authenticate('bearer', {session: false});
+const ensureAuthenticated: RequestHandler = (req, res, next) => {
+  // check session (i.e., GitHub, etc.)
+  if (req.isAuthenticated && req.isAuthenticated()) {
+    next();
+  } else {
+    bearerAuthentication(req, res, next);
+  }
+};
+app.get('/auth/tokens', ensureAuthenticated,
+        (req, res) => getAllApiTokenNames((req.user as IUser).gotandaId).then(ret => res.json(ret)));
+app.delete('/auth/tokens', ensureAuthenticated,
+           (req, res) => deleteAllApiTokens((req.user as IUser).gotandaId).then(ret => res.json(ret)));
+app.delete('/auth/token/:name', ensureAuthenticated,
+           (req, res) => deleteApiToken((req.user as IUser).gotandaId, req.params.name).then(ret => res.json(ret)));
+app.get('/auth/token/:name', ensureAuthenticated,
+        (req, res) => createApiToken((req.user as IUser).gotandaId, req.params.name).then(ret => res.json(ret)))
+
 app.get('/auth/github', ensureUnauthenticated, passport.authenticate('github'));
 app.get('/auth/github/callback', ensureUnauthenticated, passport.authenticate('github', {failureRedirect: '/'}),
         (req, res) => res.redirect('/'));
@@ -65,8 +97,7 @@ app.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
 });
-app.get('/personal', require('connect-ensure-login').ensureLoggedIn('/'),
-        (req, res) => res.send(`You must be logged in! <a href="/">Go back</a>`));
+app.get('/personal', ensureAuthenticated, (req, res) => res.send(`You're logged in! <a href="/">Go back</a>`));
 app.get('/loginstatus', (req, res) => res.status(req.isAuthenticated() ? 200 : 401).end());
 
 // Gotanda syncer
@@ -75,8 +106,8 @@ const SyncPayload = t.type({
   lastSharedId: t.string,
   newEvents: t.array(t.string),
 });
-// export type ISyncPayload = t.TypeOf<typeof SyncPayload>;
-app.post('/sync/:app', require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
+// export type ISyncPayload = t.TypeOf<typeof SyncPayload>; // export this if we want to use it in frontend
+app.post('/sync/:app', ensureAuthenticated, (req, res) => {
   const userId = req.user && (req.user as IUser).gotandaId;
   const {app} = req.params;
   if (!(userId && app && !app.includes('/'))) {
--- users.ts
+++ users.ts
@@ -11,7 +11,7 @@ export type Db = ReturnType<typeof levelup>;
 const db: Db = (require('level'))('gotanda-users-db', {valueEncoding: 'json'});
 
 const User = t.intersection([
-  t.type({gotandaId: t.string}),
+  t.type({gotandaId: t.string, apiTokens: t.array(t.type({token: t.string, name: t.string}))}),
   t.partial({github: t.UnknownRecord}), // we'll later allow sign up with others SSO, not just GitHub
 ]);
 export type IUser = t.TypeOf<typeof User>;
@@ -36,23 +36,26 @@ export async function getUser(key: string): Promise<IUser|undefined> {
 The database stores the following keys and values:
 - `gotanda-<random string not including slash>` => IUser object (i.e., a Gotanda user record)
 - `github-<GitHub id>` => a string which is a Gotanda user record's key (i.e., `gotanda-<random string>`)
+- `token-<random string>` => ditto (a string which is the key to a IUser)
 
 When we get a GitHub profile, we look up whether `github-<id>` exists in the database. If it does, we fetch its value
 and in turn use that as a key to look up the Gotanda user record.
 */
+
+async function base64urlRandom(nbytes: number) {
+  return (await randomBytes(nbytes)).toString('base64').replace(/\//g, '_').replace(/\+/g, '-');
+}
+
 export async function findOrCreateGithub(profile: GitHubStrategy.Profile): Promise<IUser> {
   const githubId = `github-${profile.id}`;
   const hit = await getKey(githubId);
   if (hit) {
     // GitHub user is also a Gotanda user!
     const existing = await getUser(hit);
-    if (existing) {
-      return existing;
-    } else {
-      // We had a record of this GitHub user but not the Gotanda user it pointed to?
-      // Something very strange must have happened but we can deal with it.
-      console.error(`${githubId} points to ${hit} which doesn't exist. Creating.`);
-    }
+    if (existing) { return existing; }
+    // We had a record of this GitHub user but not the Gotanda user it pointed to?
+    // Something very strange must have happened but we can deal with it.
+    console.error(`${githubId} points to ${hit} which doesn't exist. Creating.`);
   }
   // new user
 
@@ -60,13 +63,79 @@ export async function findOrCreateGithub(profile: GitHubStrategy.Profile): Promi
   let newGotandaId = ''; // Not used for return logins
   while (!newGotandaId) {
     // RFC 4648 §5: base64url
-    const attempt = (await randomBytes(8)).toString('base64').replace(/\//g, '_').replace(/\+/g, '-');
+    const attempt = await base64urlRandom(9);
     if (!(await getKey(attempt))) { newGotandaId = 'gotanda-' + attempt; }
   }
 
   // unclear why I need {...profile}, but just `profile` makes typescript unhappy
-  const ret: IUser = {github: {...profile}, gotandaId: newGotandaId};
+  const ret: IUser = {github: {...profile}, gotandaId: newGotandaId, apiTokens: []};
 
   await db.batch().put(githubId, newGotandaId).put(newGotandaId, ret).write();
   return ret;
+}
+
+export async function findApiToken(token: string): Promise<IUser|undefined> {
+  const hit = await getKey(token);
+  if (hit) {
+    const existing = await getUser(hit);
+    if (existing && existing.apiTokens && existing.apiTokens.find(o => o.token === token)) { return existing; }
+    console.error(`API token ${token} points to user either non-existent or who deleted this token. Maybe delete it?`);
+  }
+  return undefined;
+}
+
+export async function createApiToken(gotandaId: string, name: string): Promise<string|undefined> {
+  const newToken = 'token-' + await base64urlRandom(21);
+  // I should check to make sure there's no collisions but seriously that's so unrealistic. E.g., BT DHT just treats
+  // randomly-generated 160-bit (20-byte) buffers as "globally unique".
+
+  // make sure input ID is valid
+  const user = await getUser(gotandaId);
+  if (!user) { return undefined; }
+
+  // add this token
+  user.apiTokens.push({token: newToken, name});
+  // commit to db
+  await db.batch().put(newToken, gotandaId).put(gotandaId, user).write();
+  return newToken;
+}
+
+export async function deleteApiToken(gotandaId: string, name: string): Promise<boolean> {
+  const user = await getUser(gotandaId);
+  if (!user) { return false; }
+
+  const batch = db.batch();
+  {
+    const newApiTokens: typeof user.apiTokens = [];
+    for (const o of user.apiTokens) {
+      if (o.name === name) {
+        batch.del(o.token)
+      } else {
+        newApiTokens.push(o);
+      }
+    }
+    // if `name` isn't found, don't touch the database
+    if (newApiTokens.length === user.apiTokens.length) { return false; }
+    user.apiTokens = newApiTokens;
+  }
+
+  await batch.put(gotandaId, user).write();
+  return true;
+}
+
+export async function deleteAllApiTokens(gotandaId: string): Promise<boolean> {
+  const user = await getUser(gotandaId);
+  if (!user) { return false; }
+  const batch = db.batch();
+  for (const token of user.apiTokens) { batch.del(token.token); }
+  user.apiTokens = [];
+  batch.put(gotandaId, user);
+  await batch.write();
+  return true;
+}
+
+export async function getAllApiTokenNames(gotandaId: string): Promise<string[]> {
+  const user = await getUser(gotandaId);
+  if (!user) { return []; }
+  return user.apiTokens.map(o => o.name);
 }
